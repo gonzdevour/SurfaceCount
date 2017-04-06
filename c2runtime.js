cd85fd07adc9c4876baf75703425f66c854d232c
gonzdevour <jyunru.li@gmail.com>
2017-04-07 02:50:20 +0800

    a

--- c2runtime.js
+++ c2runtime.js
@@ -16435,90 +16435,6 @@ cr.plugins_.Function = function(runtime)
 }());
 ;
 ;
-cr.plugins_.Rex_PromptWrap = function(runtime)
-{
-	this.runtime = runtime;
-};
-(function ()
-{
-	var input_text = "";
-	var pluginProto = cr.plugins_.Rex_PromptWrap.prototype;
-	pluginProto.Type = function(plugin)
-	{
-		this.plugin = plugin;
-		this.runtime = plugin.runtime;
-	};
-	var typeProto = pluginProto.Type.prototype;
-	typeProto.onCreate = function()
-	{
-	};
-	pluginProto.Instance = function(type)
-	{
-		this.type = type;
-		this.runtime = type.runtime;
-	};
-	var instanceProto = pluginProto.Instance.prototype;
-	instanceProto.onCreate = function()
-	{
-        this.enable_wrap = (this.properties[0] === 1);
-		var self = this;
-		if (this.runtime.isCocoonJs && this.enable_wrap)
-		{
-			CocoonJS["App"]["onTextDialogFinished"].addEventListener(function(text) {
-				input_text = text;
-				self.runtime.trigger(cr.plugins_.Rex_PromptWrap.prototype.cnds.OnKeyboardOK, self);
-			});
-			CocoonJS["App"]["onTextDialogCancelled"].addEventListener(function() {
-				self.runtime.trigger(cr.plugins_.Rex_PromptWrap.prototype.cnds.OnKeyboardCancelled, self);
-			});
-        }
-	};
-	instanceProto.cocoonJS_PromptKeyboard = function (title_, message_, initial_, type_, canceltext_, oktext_)
-	{
-		if (!this.runtime.isCocoonJs)
-			return;
-		var typestr = ["text", "num", "phone", "email", "url"][type_];
-		CocoonJS["App"]["showTextDialog"](title_, message_, initial_, typestr, canceltext_, oktext_);
-	};
-	instanceProto.web_prompt = function (title_, message_, initial_, type_, canceltext_, oktext_)
-	{
-		var retval = prompt(title_, initial_);
-        if(retval != null)
-        {
-            input_text = retval;
-            this.runtime.trigger(cr.plugins_.Rex_PromptWrap.prototype.cnds.OnKeyboardOK, this);
-        }
-        else
-            this.runtime.trigger(cr.plugins_.Rex_PromptWrap.prototype.cnds.OnKeyboardCancelled, this);
-	};
-	function Cnds() {};
-	pluginProto.cnds = new Cnds();
-	Cnds.prototype.OnKeyboardCancelled = function ()
-	{
-		return true;
-	};
-	Cnds.prototype.OnKeyboardOK = function ()
-	{
-		return true;
-	};
-	function Acts() {};
-	pluginProto.acts = new Acts();
-	Acts.prototype.PromptKeyboard = function (title_, message_, initial_, type_, canceltext_, oktext_)
-	{
-		if (this.runtime.isCocoonJs && this.enable_wrap)
-			this.cocoonJS_PromptKeyboard(title_, message_, initial_, type_, canceltext_, oktext_);
-        else
-            this.web_prompt(title_, message_, initial_, type_, canceltext_, oktext_);
-	};
-	function Exps() {};
-	pluginProto.exps = new Exps();
-	Exps.prototype.InputText = function (ret)
-	{
-		ret.set_string(input_text);
-	};
-}());
-;
-;
 cr.plugins_.Sprite = function(runtime)
 {
 	this.runtime = runtime;
@@ -20392,7 +20308,6 @@ cr.plugins_.rex_TouchWrap = function(runtime)
     };
 }());
 cr.getObjectRefTable = function () { return [
-	cr.plugins_.Rex_PromptWrap,
 	cr.plugins_.rex_TouchWrap,
 	cr.plugins_.Text,
 	cr.plugins_.TextBox,
@@ -20411,10 +20326,10 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.TextBox.prototype.acts.SetText,
 	cr.plugins_.cranberrygame_CordovaDialog.prototype.exps.PromptInput,
 	cr.system_object.prototype.exps["float"],
+	cr.plugins_.TextBox.prototype.exps.Text,
 	cr.plugins_.Function.prototype.acts.CallFunction,
 	cr.plugins_.Function.prototype.cnds.OnFunction,
 	cr.plugins_.Text.prototype.acts.SetText,
-	cr.system_object.prototype.cnds.EveryTick,
 	cr.plugins_.Button.prototype.cnds.OnClicked,
 	cr.system_object.prototype.acts.GoToLayoutByName,
 	cr.plugins_.Browser.prototype.cnds.OnBackButton,
